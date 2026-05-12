0aeb91d0e3ac68d8ca60bb6aaac1187c2e89920e
Hunter.zong <hunter.zong@HunterzongdeMac-mini.local>
2026-05-12 14:15:33 +0800

    清理临时文件 - 删除重复文件、构建缓存、DS_Store等

--- DerivedData2/Build/Intermediates.noindex/Pods.build/Release-iphoneos/CapacitorCordova.build/Objects-normal/arm64/CDVViewController.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/hunter.zong/Work/Test\ Pj/贪食蛇\ iosAPP/DerivedData2/Build/Intermediates.noindex/Pods.build/Release-iphoneos/CapacitorCordova.build/module.modulemap \
-  /Users/hunter.zong/Work/Test\ Pj/node_modules/@capacitor/ios/CapacitorCordova/CapacitorCordova/Classes/Public/CDVViewController.m \
-  /Users/hunter.zong/Work/Test\ Pj/贪食蛇\ iosAPP/App/Pods/Target\ Support\ Files/CapacitorCordova/CapacitorCordova-prefix.pch \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/UIKit.framework/Modules/module.modulemap \
-  /Users/hunter.zong/Work/Test\ Pj/node_modules/@capacitor/ios/CapacitorCordova/CapacitorCordova/Classes/Public/CDVViewController.h
--- DerivedData2/Build/Intermediates.noindex/Pods.build/Release-iphoneos/Pods-App.build/Objects-normal/arm64/Pods-App-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/hunter.zong/Work/Test\ Pj/贪食蛇\ iosAPP/DerivedData2/Build/Intermediates.noindex/Pods.build/Release-iphoneos/Pods-App.build/module.modulemap \
-  /Users/hunter.zong/Work/Test\ Pj/贪食蛇\ iosAPP/App/Pods/Target\ Support\ Files/Pods-App/Pods-App-dummy.m \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
